f3e10e7f22408361ed0745ad11dc2d2cd023c132
Kwenten <149977488+Kwenten@users.noreply.github.com>
2024-12-13 11:00:20 +0700

    Add files via upload

---
+++ Lab6N2.js
@@ -0,0 +1,31 @@
+let N = 3;
+
+let M = 4;
+
+let arr = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,18,11,12]
+];
+
+let new_arr = [];
+
+let count = 0;
+
+function array_new(k, B, array) {
+    let count1 = 0;
+    for (let i = 0; i <= B - 1; i++) {
+        count1 += array[i][k];
+    }
+    return count1;
+}
+
+for (let j = 1; j <= M - 1; j = j + 2) {
+    let new_arr_mini = ['Сумма ' + (j + 1) + ' столбца = '];
+    count = array_new(j, N, arr);
+    new_arr_mini.push(count + ' ');
+    new_arr.push(new_arr_mini);
+    count = 0;
+}
+
+alert(new_arr.flat(Infinity).join(" "));
